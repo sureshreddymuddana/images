a6df7815eed1df1bea23ade36a10bc8db910626c
sureshreddymuddana <sureshreddy338@gmail.com>
2017-05-17 12:58:50 +0530

    new change

--- script.sh
+++ script.sh
@@ -1,11 +1,3 @@
 echo "hello world this is my first Docker file"
 echo "hello world this is my first Docker file"
-echo "hello world this is my first Docker file"
-echo "hello world this is my first Docker file"
-echo "hello world this is my first Docker file"
-echo "hello world this is my first Docker file"
-echo "hello world this is my first Docker file"
-echo "hello world this is my first Docker file"
-echo "hello world this is my first Docker file"
-echo "hello world this is my first Docker file"
 
